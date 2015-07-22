e3b16b58f172782fc46c49d44d9140734f666305
Kevin Jiang <kittymiky@gmail.com>
2015-07-22 12:52:59 +0800

    Update init.el

--- .emacs.d/lisp/init.el
+++ .emacs.d/lisp/init.el
@@ -4,6 +4,13 @@
 
 
 
+;; keep cursor at same position when scrolling
+(setq scroll-preserve-screen-position 1)
+;; scroll window up/down by one line
+(global-set-key (kbd "M-n") (kbd "C-u 1 C-v"))
+(global-set-key (kbd "M-p") (kbd "C-u 1 M-v"))
+
+
 ;; 设置扩展路径
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
